1d3c9f82f05e9a6733c51874e94590ff37d55fd8
mikewng <156055657+mikewng@users.noreply.github.com>
2025-08-06 22:00:33 -0400

    managers

---
+++ src/app/engine/game/gamestate.ts
@@ -0,0 +1,3 @@
+interface GameState {
+    phase: "player" | "enemy"
+}
---
+++ src/app/engine/managers/unitmanager.ts
@@ -0,0 +1,30 @@
+class UnitManager {
+    private units: Map<string, Unit> = new Map();
+
+    /**
+     * TODO
+     * - Add Unit(s)
+     * - Remove Unit(s)
+     * - Move Unit
+     * - Get Unit by ID
+     * - Get Units by Faction
+     * - Get Unit at XY position
+     */
+
+    addUnit(unit: Unit) {
+        this.units.set(unit.id, unit);
+    }
+
+    removeUnit(id: string) {
+        this.units.delete(id);
+    }
+
+    moveUnit(id: string, x: number, y: number) {
+        const unit = this.units.get(id);
+        if (unit) {
+            unit.position = { x, y }
+        }
+    }
+
+    
+}
